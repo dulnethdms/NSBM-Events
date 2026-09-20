a6d6cbbc64df5024ab32663fa27941e23f5a436e
c1dk8 <shaunkalexer@gmail.com>
2026-09-20 11:19:47 +0530

    c1d

--- includes/footer.php
+++ includes/footer.php
@@ -9,13 +9,13 @@
                     <span class="navbar-brand-badge small">NSBM</span>
                     <span class="fw-bold text-white">EventHub</span>
                 </div>
-                <p class="small text-muted mb-0">University Event Planning & Scheduling System &copy; <?php echo date('Y'); ?> NSBM Green University.</p>
+                <p class="small text-white mb-0">University Event Planning & Scheduling System &copy; <?php echo date('Y'); ?> NSBM Green University.</p>
             </div>
             <div class="col-md-6 text-center text-md-end">
                 <ul class="list-inline mb-0 small">
-                    <li class="list-inline-item me-3"><a href="../index.php" class="text-muted text-decoration-none">Home</a></li>
-                    <li class="list-inline-item me-3"><a href="../auth/login.php" class="text-muted text-decoration-none">Portal Login</a></li>
-                    <li class="list-inline-item"><a href="../auth/register.php" class="text-muted text-decoration-none">Student Registration</a></li>
+                    <li class="list-inline-item me-3"><a href="../index.php" class="text-white text-decoration-none">Home</a></li>
+                    <li class="list-inline-item me-3"><a href="../auth/login.php" class="text-white text-decoration-none">Portal Login</a></li>
+                    <li class="list-inline-item"><a href="../auth/register.php" class="text-white text-decoration-none">Student Registration</a></li>
                 </ul>
             </div>
         </div>
